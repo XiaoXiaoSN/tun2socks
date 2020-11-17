2212b693b6366073c54f86d2b742f82503b869ae
xjasonlyu <xjasonlyu@gmail.com>
2020-11-17 21:06:43 +0800

    stack

---
+++ stack/icmp.go
@@ -0,0 +1,40 @@
+package stack
+
+import (
+	"fmt"
+	"net"
+
+	"gvisor.dev/gvisor/pkg/tcpip"
+)
+
+func withICMPHandler() Option {
+	return func(s *Stack) error {
+		// Add default route table for IPv4 and IPv6.
+		// This will handle all incoming ICMP packets.
+		s.SetRouteTable([]tcpip.Route{
+			{
+				Destination: mustSubnet("0.0.0.0/0"),
+				NIC:         s.nicID,
+			},
+			{
+				Destination: mustSubnet("::/0"),
+				NIC:         s.nicID,
+			},
+		})
+		return nil
+	}
+}
+
+// mustSubnet returns tcpip.Subnet from CIDR string.
+func mustSubnet(s string) tcpip.Subnet {
+	_, ipNet, err := net.ParseCIDR(s)
+	if err != nil {
+		panic(fmt.Errorf("unable to ParseCIDR(%s): %w", s, err))
+	}
+
+	subnet, err := tcpip.NewSubnet(tcpip.Address(ipNet.IP), tcpip.AddressMask(ipNet.Mask))
+	if err != nil {
+		panic(fmt.Errorf("unable to NewSubnet(%s): %w", ipNet, err))
+	}
+	return subnet
+}
