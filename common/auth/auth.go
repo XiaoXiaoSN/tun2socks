b7ba2218fafda9f1e1502151c0741a693e5b0401
xjasonlyu <xjasonlyu@gmail.com>
2020-11-23 17:28:17 +0800

    socks5

---
+++ common/auth/auth.go
@@ -0,0 +1,50 @@
+package auth
+
+// Ref: github.com/Dreamacro/clash/component/auth
+
+import (
+	"sync"
+)
+
+type Authenticator interface {
+	Verify(user string, pass string) bool
+	Users() []string
+}
+
+type User struct {
+	User string
+	Pass string
+}
+
+var _ Authenticator = (*inMemoryAuthenticator)(nil)
+
+type inMemoryAuthenticator struct {
+	storage   *sync.Map
+	usernames []string
+}
+
+func (au *inMemoryAuthenticator) Verify(user string, pass string) bool {
+	realPass, ok := au.storage.Load(user)
+	return ok && realPass == pass
+}
+
+func (au *inMemoryAuthenticator) Users() []string { return au.usernames }
+
+func NewAuthenticator(users []User) Authenticator {
+	if len(users) == 0 {
+		return nil
+	}
+
+	au := &inMemoryAuthenticator{storage: &sync.Map{}}
+	for _, user := range users {
+		au.storage.Store(user.User, user.Pass)
+	}
+	usernames := make([]string, 0, len(users))
+	au.storage.Range(func(key, value interface{}) bool {
+		usernames = append(usernames, key.(string))
+		return true
+	})
+	au.usernames = usernames
+
+	return au
+}
